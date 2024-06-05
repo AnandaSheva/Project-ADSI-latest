134d37c7eff75f907e199e52b13446015f59337a
Ananda Sheva Hidayat <sheva1257@gmail.com>
2024-06-05 19:16:08 +0700

    Menambahkan dynamic scroll

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -365,11 +365,22 @@
       </div>
     </div>
 
+
     <!-- Bootstrap core JavaScript-->
     <script src="Assets/vendor/jquery/jquery.min.js"></script>
     <script src="Assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
+    <!-- Core plugin JavaScript-->
+    <script src="Assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+
     <!-- Custom scripts for all pages-->
     <script src="Assets/js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="Assets/vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="Assets/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="Assets/js/demo/datatables-demo.js"></script>
 </body>
 </html>
